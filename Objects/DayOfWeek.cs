0ca0f534abc6cacb56dc58213563d7b95569f706
Anne Belka and Steve Burton <anne+steve@>
2016-12-01 16:10:20 -0800

    add test for final formula

--- Objects/DayOfWeek.cs
+++ Objects/DayOfWeek.cs
@@ -6,16 +6,15 @@ namespace DayFinder.Objects
 	public class DayOfWeek
 	{
 		public int Day {get; set;}
-		public int Month {get; set;}
+		public double Month {get; set;}
 		public int Year {get; set;}
 
 
-		public DayOfWeek(int day, int month, int year, int century)
+		public DayOfWeek( int day, double month, int year)
 		{
 			this.Day = day;
 			this.Month = month;
 			this.Year = year;
-
 		}
 
 		public string DayFinder()
@@ -24,10 +23,13 @@ namespace DayFinder.Objects
 
 			string yearEnd = yearString.Substring(2,2);
 			string yearStart = yearString.Substring(0,2);
+			double yearStartInt = int.Parse(yearStart);
 			string yearEndNew = "";
+			string result = "";
+			double yearEndInt = 0;
 			if (this.Month == 1 || this.Month == 2)
 			{
-					int yearEndInt = int.Parse(yearEnd);
+					yearEndInt = int.Parse(yearEnd);
 					yearEndInt = yearEndInt - 1;
 					yearEndNew = yearEndInt.ToString();
 			}
@@ -37,7 +39,51 @@ namespace DayFinder.Objects
 				this.Month += 12;
 			}
 			string monthString = this.Month.ToString();
-			return monthString;
+			double phrase1 = Math.Floor(2.6*this.Month - .2);
+			double phrase2 = Math.Floor(yearEndInt/4);
+			double phrase3 = Math.Floor((yearStartInt)/4);
+		  double math = (this.Day + phrase1 + (yearEndInt) + phrase2 + phrase3 - (2*(yearStartInt)))%7;
+			Console.WriteLine("this.Day" + this.Day);
+			Console.WriteLine("this.Month" + this.Month);
+			Console.WriteLine("yearEndInt" + yearEndInt);
+			Console.WriteLine("yearStartInt" + yearStartInt);
+			Console.WriteLine(math);
+
+			if(math < 0)
+			{
+				math +=7;
+			}
+				Console.WriteLine(math);
+				if(math == 0)
+				{
+					result = "Sunday";
+				}
+				else if(math == 1)
+				{
+					result = "Monday";
+				}
+				else if(math == 2)
+				{
+					result = "Tuesday";
+				}
+				else if(math == 3)
+				{
+					result = "Wednesday";
+				}
+				else if(math == 4)
+				{
+					result = "Thursday";
+				}
+				else if(math == 5)
+				{
+					result = "Friday";
+				}
+				else if(math == 6)
+				{
+					result = "Saturday";
+				}
+
+			return result;
 		}
 	}
 }
--- Tests/DayOfWeekTest.cs
+++ Tests/DayOfWeekTest.cs
@@ -7,35 +7,60 @@ namespace DayFinder.Objects
     // [Fact]
     // public void DayFinder_SeparateYear_year()
     // {
-    //   string date = "2016";
-    //   int dateInt = int.Parse(date);
+    //   string year = "2016";
+    //   int yearInt = int.Parse(year);
     //   string month = "01";
     //   int monthInt = int.Parse(month);
-    //   DayOfWeek newDayOfWeek = new DayOfWeek(0,monthInt,dateInt,0);
+    //   DayOfWeek newDayOfWeek = new DayOfWeek(0,monthInt,yearInt,0);
     //
     //   Assert.Equal("15", newDayOfWeek.DayFinder());
     // }
     // [Fact]
     // public void DayFinder_ShiftYear_year()
     // {
-    //   string date = "2016";
+    //   string year = "2016";
     //   string month = "01";
     //   int monthInt = int.Parse(month);
-    //   int dateInt = int.Parse(date);
-    //   DayOfWeek newDayOfWeek = new DayOfWeek(0,monthInt,dateInt,0);
+    //   int yearInt = int.Parse(year);
+    //   DayOfWeek newDayOfWeek = new DayOfWeek(0,monthInt,yearInt,0);
     //
     //   Assert.Equal("15", newDayOfWeek.DayFinder());
     // }
+    // [Fact]
+    // public void DayFinder_ShiftMonth_month()
+    // {
+    //   string year = "2016";
+    //   string month = "1";
+    //   int monthInt = int.Parse(month);
+    //   int yearInt = int.Parse(year);
+    //   DayOfWeek newDayOfWeek = new DayOfWeek(0,monthInt,yearInt,0);
+    //
+    //   Assert.Equal("11", newDayOfWeek.DayFinder());
+    // }
+    // [Fact]
+    // public void DayFinder_DayToNumber_day()
+    // {
+    //   string year = "2016";
+    //   string month = "1";
+    //   string dayOfWeek = "Saturday";
+    //   int monthInt = int.Parse(month);
+    //   int yearInt = int.Parse(year);
+    //   DayOfWeek newDayOfWeek = new DayOfWeek(dayOfWeek,monthInt,yearInt,0);
+    //
+    //   Assert.Equal("6", newDayOfWeek.DayFinder());
+    // }
     [Fact]
-    public void DayFinder_ShiftMonth_month()
+    public void DayFinder_DateToDay_day()
     {
-      string date = "2016";
-      string month = "1";
-      int monthInt = int.Parse(month);
-      int dateInt = int.Parse(date);
-      DayOfWeek newDayOfWeek = new DayOfWeek(0,monthInt,dateInt,0);
+      string year = "1973";
+      string month = "12";
+      int dayOfMonth = 2;
 
-      Assert.Equal("11", newDayOfWeek.DayFinder());
+      int monthInt = int.Parse(month);
+      int yearInt = int.Parse(year);
+      DayOfWeek newDayOfWeek = new DayOfWeek(dayOfMonth,monthInt,yearInt);
+
+      Assert.Equal("Sunday", newDayOfWeek.DayFinder());
     }
   }
 }
